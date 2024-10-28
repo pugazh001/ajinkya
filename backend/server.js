8688bc949525fb8b0134fe29e00118c4dfac20b8
PUGAZHENTHI S <132038855+pugazh001@users.noreply.github.com>
2024-10-28 22:25:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
-const app = express();
+
 const cors = require('cors'); // Import the cors package
+
+
+const app = express();
 
 // Use CORS middleware
 app.use(cors());
